f70042bb76285e91efa4df1000901af3f1155d48
ASAN-INJU <jjukssam755@gmail.com>
2026-06-07 18:52:18 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 const CACHE_NAME = "asan-inju-v1";
 
+// 캐시에 저장할 기본 파일
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json"
 ];
 
+// 설치 시 캐시 저장
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -14,6 +16,7 @@ self.addEventListener("install", event => {
   );
 });
 
+// 요청이 있을 때 캐시 우선 사용
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
