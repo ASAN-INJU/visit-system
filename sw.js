6cc11465162951def05d5081bcd058dfd523a862
ASAN-INJU <jjukssam755@gmail.com>
2026-07-04 01:47:57 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='inju-v3-alpha-1';
-const FILES=['./','./index.html','./css/style.css','./js/app.js','./data/geolmaeri.json','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
