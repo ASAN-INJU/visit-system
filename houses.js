ebdff37600352d0f2f26ca4ace8ea1770bb75ccf
ASAN-INJU <jjukssam755@gmail.com>
2026-07-04 19:17:30 +0900

    Add houses data structure with details

---
+++ houses.js
@@ -0,0 +1,33 @@
+const houses = [
+
+{
+number:"89-10",
+name:"주찬양교회",
+address:"걸매길 89-10"
+},
+
+{
+number:"89-9",
+name:"",
+address:"걸매길 89-9"
+},
+
+{
+number:"89-6",
+name:"",
+address:"걸매길 89-6"
+},
+
+{
+number:"93",
+name:"걸매리 마을회관",
+address:"걸매길 93"
+},
+
+{
+number:"95",
+name:"걸매리 주민체육실",
+address:"걸매길 95"
+}
+
+];
